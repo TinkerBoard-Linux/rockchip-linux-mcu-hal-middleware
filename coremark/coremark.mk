4d397ebb16e8fc97e2eba96145f6ea80e4faf01a
Tao Huang <huangtao@rock-chips.com>
2021-04-23 19:56:18 +0800

    coremark: Add coremark.mk
    Signed-off-by: Tao Huang <huangtao@rock-chips.com>
    Change-Id: I7cb529acf7365caa548ce22cca75a86e195328c4

---
+++ coremark/coremark.mk
@@ -0,0 +1,13 @@
+# SPDX-License-Identifier: BSD-3-Clause */
+
+# Copyright (c) 2021 Rockchip Electronics Co., Ltd.
+
+COREMARK_PATH=$(HAL_PATH)/middleware/coremark
+
+INCLUDES += \
+-I"$(COREMARK_PATH)" \
+-I"$(COREMARK_PATH)/barebones" \
+
+SRC_DIRS += \
+    $(COREMARK_PATH) \
+    $(COREMARK_PATH)/barebones \
