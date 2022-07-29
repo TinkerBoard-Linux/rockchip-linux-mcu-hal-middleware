72e8714e4cfa8bc39bd3eb384785417b07d849de
Steven Liu <steven.liu@rock-chips.com>
2022-07-29 19:04:06 +0800

    rpmsg-lite: add RL_EPT_SIZE
    Signed-off-by: Steven Liu <steven.liu@rock-chips.com>
    Change-Id: I992e7414825594b353be07c5726dc11a0e92f2a0

--- rpmsg-lite/lib/include/platform/RK3308/rpmsg_config.h
+++ rpmsg-lite/lib/include/platform/RK3308/rpmsg_config.h
@@ -7,6 +7,8 @@
 
 #define RL_BUFFER_PAYLOAD_SIZE (496U)
 #define RL_BUFFER_COUNT (64U)
+/* endpoint size is formed by payload and struct rpmsg_std_hdr */
+#define RL_EPT_SIZE (RL_BUFFER_PAYLOAD_SIZE + 16UL)
 
 #define RL_MAX_INSTANCE_NUM (12U)
 #define RL_PLATFORM_HIGHEST_LINK_ID     (0xFFU)
