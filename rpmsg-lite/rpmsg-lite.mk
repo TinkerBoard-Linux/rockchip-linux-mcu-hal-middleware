d5a39aef47bbd8e7c0866e559d385ecd92a74c61
Steven Liu <steven.liu@rock-chips.com>
2022-10-12 16:37:08 +0800

    rpmsg-lite: test: add rpmsg performance test
    Signed-off-by: Steven Liu <steven.liu@rock-chips.com>
    Change-Id: I6ffa590681a0fc8fc6c26d69d998be94b445c82b

--- rpmsg-lite/rpmsg-lite.mk
+++ rpmsg-lite/rpmsg-lite.mk
@@ -8,12 +8,14 @@ INCLUDES += \
 -I"$(RPMSG_LITE_PATH)/include" \
 -I"$(RPMSG_LITE_PATH)/include/environment/bm" \
 -I"$(RPMSG_LITE_PATH)/include/platform/$(SOC)" \
+-I"$(RPMSG_LITE_PATH)/test" \
 
 SRC_DIRS += \
     $(RPMSG_LITE_PATH)/common \
     $(RPMSG_LITE_PATH)/rpmsg_lite \
     $(RPMSG_LITE_PATH)/rpmsg_lite/porting/platform/$(SOC) \
     $(RPMSG_LITE_PATH)/virtio \
+    $(RPMSG_LITE_PATH)/test \
 
 SRCS += \
     $(RPMSG_LITE_PATH)/rpmsg_lite/porting/environment/rpmsg_env_bm.c \
