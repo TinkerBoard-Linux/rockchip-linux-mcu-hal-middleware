31cb38b8ff6274ef5be205537c9ca55a74b0ab49
Steven Liu <steven.liu@rock-chips.com>
2022-11-29 11:50:38 +0800

    benchmark: only open coremark as default test
    Signed-off-by: Steven Liu <steven.liu@rock-chips.com>
    Change-Id: I26b1d8d07751cbc55898ccfdf7638c41dc6b479c

--- benchmark/benchmark.mk
+++ benchmark/benchmark.mk
@@ -8,12 +8,12 @@ INCLUDES += \
 -I"$(BENCHMARK_PATH)" \
 -I"$(BENCHMARK_PATH)/coremark" \
 -I"$(BENCHMARK_PATH)/coremark/barebones" \
--I"$(BENCHMARK_PATH)/linpack" \
--I"$(BENCHMARK_PATH)/tinymembench" \
+# -I"$(BENCHMARK_PATH)/linpack" \
+# -I"$(BENCHMARK_PATH)/tinymembench" \
 
 SRC_DIRS += \
     $(BENCHMARK_PATH) \
     $(BENCHMARK_PATH)/coremark \
     $(BENCHMARK_PATH)/coremark/barebones \
-    $(BENCHMARK_PATH)/linpack \
-    $(BENCHMARK_PATH)/tinymembench \
+#    $(BENCHMARK_PATH)/linpack \
+#    $(BENCHMARK_PATH)/tinymembench \
