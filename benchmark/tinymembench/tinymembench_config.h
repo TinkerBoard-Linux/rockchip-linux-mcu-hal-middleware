b4a308fae8c1a34f485aa5748882797b3c5617b4
Steven Liu <steven.liu@rock-chips.com>
2022-12-12 11:24:38 +0800

    benchmark: tinymembench: add MCU configuration
    Signed-off-by: Steven Liu <steven.liu@rock-chips.com>
    Change-Id: I609b8faa0e4e2ab72b825b07e73c2a9dc0c6e83c

--- benchmark/tinymembench/tinymembench_config.h
+++ benchmark/tinymembench/tinymembench_config.h
@@ -5,18 +5,15 @@
 #ifndef TINYMEMBENCH_CONFIG_H_
 #define TINYMEMBENCH_CONFIG_H_
 
+/* Configuration for ARM Cortex-A55 */
 #define TINYMEMBENCH_SIZE (4 * 1024 * 1024)
-
 #define ALIGN_PADDING 0x100000
-
-/*
- * 32 byte cacheline size:
- * 1.Rockchip MCU Subsystem ARM Cortex-M0
- *
- * 64 byte cacheline size:
- * 1.ARM Cortex-A55
- */
 #define CACHE_LINE_SIZE 64
+
+/* Configuration for MCU */
+//#define TINYMEMBENCH_SIZE (64 * 1024)
+//#define ALIGN_PADDING 0x1000
+//#define CACHE_LINE_SIZE 32
 
 #define TINYMEMBENCH_CLOCKS_PER_SEC 1000
 
